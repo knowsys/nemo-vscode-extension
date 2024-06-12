8ebfa0631d93acd0762c43c3a53414acfbdb3392
logicallangs <>
2024-06-12 13:27:41 +0100

    add extension

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# nemo-vscode-extension
-Extension to support Nemo rules in VSCode and compatible editors
+# Nemo Rule Engine Language Support
+
+Features:
+
+-   Nemo Rule File language configuration
+-   Basic syntax highlighting
+-   Syntax error diagnostics
+-   Document outline
+-   Find references of predicates, local variables, functions and aggregates
+-   Rename all occurences of predicates, local variables, functions and aggregates
+
+This editor extension works with VSIX-compatible editors such as [VSCodium](https://vscodium.com/) and [Eclipse Theia](https://theia-ide.org/).
+
+## Installation
+
+-   Install extension `.vsix` file
+
+In [web contexts](https://code.visualstudio.com/docs/editor/vscode-web#_extensions), this extension works out of the box (using web assembly). In all other contexts, one needs to setup the Nemo language server manually:
+
+-   Build the [Nemo language server]([Nemo WASM library](https://github.com/knowsys/nemo/tree/parser/nemo-language-server)) for your platform
+
+    ```
+    git clone -b parser https://github.com/knowsys/nemo.git
+    cd nemo
+    cargo build --release --bin nemo-languager-server
+    # Get path to language server binary
+    realpath target/release/nemo-language-server
+    ```
+
+-   Setup path to Nemo language server in the extension settings
+
+## Building the `.vsix` extension
+
+-   Clone the repository
+-   Build the [Nemo WASM library](https://github.com/knowsys/nemo/tree/main/nemo-wasm)
+-   Build and package the extension
+
+    ```bash
+    # Copy nemo-wasm library to the correct location
+    cp -rTv $PATH_TO_NEMO_WASM/nemoWASMWeb .
+
+    npm install
+    npm run package
+    ```
---
+++ src/node/nodeClientMain.ts
@@ -0,0 +1,184 @@
+import { access, readFile } from "fs/promises";
+import * as vscode from "vscode";
+import {
+    Executable,
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+} from "vscode-languageclient/node";
+import * as common from "../common/common";
+import { dirname, join } from "path";
+
+export async function activate(context: vscode.ExtensionContext) {
+    showStartupMessage();
+
+    await common.activate(context, createLanguageClient);
+
+    vscode.workspace.onDidChangeConfiguration((event) => {
+        if (event.affectsConfiguration("nemo.languageServerExecutablePath")) {
+            vscode.commands.executeCommand("nemo.restartLanguageServer");
+        }
+    });
+}
+
+export async function showStartupMessage() {
+    if (
+        vscode.workspace
+            .getConfiguration()
+            .get<boolean>("nemo.hideStartupMessage") === true
+    ) {
+        return;
+    }
+
+    let showMessage = false;
+
+    try {
+        const product: any = JSON.parse(
+            (
+                await readFile(
+                    join(
+                        dirname(process.execPath),
+                        "resources",
+                        "app",
+                        "product.json"
+                    )
+                )
+            ).toString()
+        );
+
+        if (
+            product.nameLong.includes("Visual Studio Code") ||
+            product.extensionsGallery.serviceUrl.startsWith(
+                "https://marketplace.visualstudio.com"
+            )
+        ) {
+            showMessage = true;
+        }
+    } catch {
+        if (process.execPath.endsWith("code")) {
+            showMessage = true;
+        }
+    }
+
+    // if (!showMessage) {
+    //     return;
+    // }
+
+    const items = [
+        "Alternatives: VSCodium",
+        "Microsoft hurting the open source editor community (1)",
+        "Microsoft hurting the open source editor community (2)",
+        "Microsoft hurting the open source editor community (3)",
+    ];
+    const result = await vscode.window.showWarningMessage(
+        "Consider switching to an open-source variant of VS Code, please!",
+        {
+            modal: true,
+            detail: "It seems that you are using a proproietary variant of VS Code. Please switch to an open source variant and let us regain control over the software we use. This message can be disabled in the settings.",
+        },
+        ...items
+    );
+
+    switch (items.indexOf(result!)) {
+        case 0:
+            vscode.env.openExternal(vscode.Uri.parse("https://vscodium.com/"));
+            break;
+        case 1:
+            vscode.env.openExternal(
+                vscode.Uri.parse(
+                    "https://github.com/microsoft/vscode/issues/31168"
+                )
+            );
+            break;
+        case 2:
+            vscode.env.openExternal(
+                vscode.Uri.parse(
+                    "https://github.com/microsoft/vscode-cpptools/issues/6388"
+                )
+            );
+            break;
+        case 3:
+            vscode.env.openExternal(
+                vscode.Uri.parse(
+                    "https://www.eclipse.org/community/eclipse_newsletter/2020/march/1.php"
+                )
+            );
+            break;
+        default:
+            break;
+    }
+}
+
+export function deactivate() {
+    common.deactivate();
+}
+
+async function getLanguageServerExecutablePath(
+    context: vscode.ExtensionContext
+): Promise<string | undefined> {
+    let executablePath = vscode.workspace
+        .getConfiguration()
+        .get<string>("nemo.languageServerExecutablePath");
+
+    if (typeof executablePath !== "string" || executablePath === "") {
+        const chosenOption = await vscode.window.showInformationMessage(
+            "Nemo language server could not be found",
+            "Setup path to executable"
+        );
+
+        if (chosenOption === "Setup path to executable") {
+            await vscode.commands.executeCommand(
+                "workbench.action.openSettings",
+                "nemo.languageServerExecutablePath"
+            );
+        }
+        return undefined;
+    }
+
+    try {
+        await access(executablePath);
+        return executablePath;
+    } catch (error: any) {
+        vscode.window.showErrorMessage(
+            "Error while starting Nemo language server: Executable could not be accessed!",
+            {
+                detail:
+                    `Error while accessing file at path ${executablePath}:\n` +
+                    error.toString(),
+            }
+        );
+        return undefined;
+    }
+}
+
+async function createLanguageClient(
+    context: vscode.ExtensionContext
+): Promise<LanguageClient | undefined> {
+    const command = await getLanguageServerExecutablePath(context);
+
+    if (command === undefined) {
+        return undefined;
+    }
+
+    const executable: Executable = {
+        command,
+    };
+
+    const serverOptions: ServerOptions = {
+        run: executable,
+        debug: executable,
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "nemo" }],
+        outputChannel: common.outputChannel,
+        traceOutputChannel: common.traceOutputChannel,
+    };
+
+    return new LanguageClient(
+        "nemo",
+        "Nemo Language Client",
+        serverOptions,
+        clientOptions
+    );
+}
