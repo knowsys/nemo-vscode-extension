bfb443f2a12cc0add59c8736b2957e21ddabc5e0
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2025-05-27 14:36:31 +0200

    Bump nixpkgs to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@ rec {
   description = "Visual Studio Code extension for the Nemo rule language";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     dream2nix = {
       url = "github:nix-community/dream2nix";
